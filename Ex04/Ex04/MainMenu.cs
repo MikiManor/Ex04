17e8e51fb2ab3cf99ef81e60d720a0bf99dfbb7e
matan <matanmaron@gmail.com>
2017-01-28 17:22:24 +0200

    11

---
+++ Ex04/Ex04/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04
+{
+    class MainMenu
+    {
+        //void AddNewMenuItem();
+        //void AddNewSubMenuItem();
+
+        void Show()
+        {
+
+            bool v_Quit = true;
+            
+            do
+            {
+                Console.Clear();
+                try
+                {
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Wrong key pressed! press any key to try again...");
+                    Console.ReadLine();
+                    throw;
+                }
+            } while (!v_Quit);
+        }
+    }
+}
---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine("See Ya!");
+            Console.ReadLine();
+        }
+    }
+}
